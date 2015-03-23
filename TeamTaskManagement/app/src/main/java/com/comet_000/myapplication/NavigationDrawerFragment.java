1e83c83b1dec1af60c104c7ee82336d69145c1d1
Lang <bnlinhlang@gmail.com>
2015-03-24 00:47:21 +0700

    Material design - navigation drawer.

--- TeamTaskManagement/app/src/main/java/com/comet_000/myapplication/NavigationDrawerFragment.java
+++ TeamTaskManagement/app/src/main/java/com/comet_000/myapplication/NavigationDrawerFragment.java
@@ -1,6 +1,8 @@
 package com.comet_000.myapplication;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
@@ -10,17 +12,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.widget.Toolbar;
 
+import java.security.Key;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NavigationDrawerFragment extends Fragment {
+
+    public static final String PREF_FILE_NAME = "testpref";
+    public static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
     private ActionBarDrawerToggle mdrawerToggle;
     private DrawerLayout mDrawerLayout;
+
+    private boolean mUserLearnedDrawer;
+    protected boolean mFromSavedInstanceState;
     public NavigationDrawerFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserLearnedDrawer = Boolean.valueOf(readFromPreferences(getActivity(), KEY_USER_LEARNED_DRAWER,"false"));
+        if(savedInstanceState!=null){
+            mFromSavedInstanceState = true;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,8 +47,7 @@ public class NavigationDrawerFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
     }
 
-    public void setUp(DrawerLayout drawerLayout, Toolbar toolbar)
-    {
+    public void setUp(DrawerLayout drawerLayout, Toolbar toolbar) {
         mDrawerLayout=drawerLayout;
         mdrawerToggle=new ActionBarDrawerToggle(getActivity(),mDrawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close)
         {
@@ -45,6 +62,15 @@ public class NavigationDrawerFragment extends Fragment {
             }
         };
         mDrawerLayout.setDrawerListener(mdrawerToggle);
-
+    }
+    public void saveToPreferences(Context context, String preferenceName, String preferenceValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+    }
+    public String readFromPreferences(Context context, String preferenceName, String preferenceValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(preferenceName,preferenceValue);
     }
 }
