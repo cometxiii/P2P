a46d3be1070ba6c34c2fe49042621c37ef0bedee
Lang <bnlinhlang@gmail.com>
2015-05-01 04:12:52 +0700

    Optimize MailManager class.

--- TeamTaskManagement/app/src/main/java/com/comet_000/myapplication/CheckingMails.java
+++ TeamTaskManagement/app/src/main/java/com/comet_000/myapplication/CheckingMails.java
@@ -9,13 +9,16 @@ import java.util.concurrent.ExecutionException;
 
 import javax.mail.Address;
 import javax.mail.AuthenticationFailedException;
+import javax.mail.Flags;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
+import javax.mail.search.AndTerm;
 import javax.mail.search.BodyTerm;
+import javax.mail.search.FlagTerm;
 
 public class CheckingMails {
     String user, password;
@@ -35,47 +38,36 @@ public class CheckingMails {
         protected String[] doInBackground(String... params) {
             Properties props = System.getProperties();
             props.setProperty("mail.store.protocol", "imaps");
-
             try {
                 Session session = Session.getDefaultInstance(props, null);
-
                 //GMail
                 System.out.println("GMail logging in..");
                 Store store = session.getStore("imaps");
                 store.connect("imap.gmail.com", user, password);
-
                 System.out.println("Connected to = "+store);
                 Folder inbox = store.getFolder("Inbox");
-                inbox.open(Folder.READ_ONLY);
+                inbox.open(Folder.READ_WRITE);
                 System.out.println("Total mails are = "+inbox.getMessageCount());
 
                 //Enter term to search here
-                final String keyword = "zfgHsj6Uyk";
 
-                //Search term
-                BodyTerm bodyTerm = new BodyTerm(keyword);
+                BodyTerm bodyTerm = new BodyTerm("<zfgHsj6Uyk>");
+                FlagTerm flagTerm = new FlagTerm(new Flags(Flags.Flag.SEEN), false);
+                AndTerm andTerm = new AndTerm(bodyTerm,flagTerm);
+                //Search
 
-                System.out.println("Searching for BodyTerm with keyword = "+keyword);
-
-                Message[] foundMessages = inbox.search(bodyTerm);
+                Message[] foundMessages = inbox.search(andTerm);
                 String[] listMessage = new String[foundMessages.length];
-                for(int i = 0 ; i < foundMessages.length ; i++)
+                for(int i = 0 ; i < foundMessages.length ; i++) {
                     listMessage[i] = foundMessages[i].getContent().toString();
-                System.out.println("Total mails found for searched term are = "+foundMessages.length);
+                }
+                inbox.setFlags(foundMessages,new Flags(Flags.Flag.SEEN),true);
                 return listMessage;
-//                return (String) foundMessages[0].getContent();
-//                System.out.println("Searching done...");
-//                System.out.println("Total mails found for searched term are = "+foundMessages.length);
-//
-//                for(Message message: foundMessages){
-//                    System.out.println("found message: "+ message.getSubject());
-//                    System.out.println("found message: "+ message.toString());
-//                    System.out.println("=====");
-//                }
-
-            }catch (MessagingException e) {
+            }
+            catch (MessagingException e) {
                 e.printStackTrace();
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
                 e.printStackTrace();
             }
             return null;
--- TeamTaskManagement/app/src/main/java/com/comet_000/myapplication/MailManager.java
+++ TeamTaskManagement/app/src/main/java/com/comet_000/myapplication/MailManager.java
@@ -7,52 +7,55 @@ import java.util.List;
  * Created by King on 27-Apr-15.
  */
 public class MailManager {
+    final String keyTag = "<zfgHsj6Uyk>";
+    final String projectTag = "<ProjectName>";
+    final String pDesTag = "<ProjectDes>";
+    final String ownerTag = "<ProjectOwner>";
+    final String invitationTag = "<Invitation>";
+    final String acceptIviTag = "<AcceptInvitation>";
+    final String senderTag = "<Sender>";
     MailManager() {
-
+    }
+    private String addHeader(String tag) {
+        return keyTag + tag;
+    }
+    private String addTag(String word, String tag) {
+        return tag + word + tag;
+    }
+    private String readTag(String message, String tag) {
+        int firstIndex = message.indexOf(tag) + message.length();
+        int lastIndex = message.lastIndexOf(tag);
+        return message.substring(firstIndex, lastIndex);
     }
     public String classifyMail(String message) {
-        message = message.replace("<zfgHsj6Uyk>","");
+        message = message.replace(keyTag,"");
         int firstID = message.indexOf(">");
-        String mailType = message.substring(1,firstID);
-        return mailType;
+        return message.substring(1,firstID);
     }
     public String makeInvitation(String projectName, String projectDes, String projectOwner) {
-        String message = "<zfgHsj6Uyk><Invitation><ProjectName>" + projectName + "<ProjectName>";
-        message += "<ProjectDes>" + projectDes + "<ProjectDes>";
-        message += "<ProjectOwner>" + projectOwner + "<ProjectOwner>";
+        String message = addHeader(invitationTag);
+        message += addTag(projectName, projectTag);
+        message += addTag(projectDes, pDesTag);
+        message += addTag(projectOwner, ownerTag);
         return message;
     }
     public String[] readInvitation(String message) {
         String[] result = new String[3];
-        int firstPN = message.indexOf("<ProjectName>") + 13;
-        int lastPN =  message.lastIndexOf("<ProjectName>");
-        String projectName = message.substring(firstPN, lastPN);
-        int firstPD = message.indexOf("<ProjectDes>") + 12;
-        int lastPD =  message.lastIndexOf("<ProjectDes>");
-        String projectDes = message.substring(firstPD, lastPD);
-        int firstPO = message.indexOf("<ProjectOwner>") + 14;
-        int lastPO =  message.lastIndexOf("<ProjectOwner>");
-        String projectOwner = message.substring(firstPO, lastPO);
-        result[0] = projectName;
-        result[1] = projectDes;
-        result[2] = projectOwner;
+        result[0] = readTag(message, projectTag);
+        result[1] = readTag(message, pDesTag);
+        result[2] = readTag(message, ownerTag);
         return result;
     }
     public String makeAcceptInvitation(String projectName, String sender) {
-        String message = "<zfgHsj6Uyk><AcceptInvitation><ProjectName>" + projectName + "<ProjectName>";
-        message += "<Sender>" + sender + "<Sender>";
+        String message = addHeader(acceptIviTag);
+        message += addTag(projectName, projectTag);
+        message += addTag(sender, senderTag);
         return message;
     }
     public String[] readAcceptInvitation(String message) {
         String[] result = new String[2];
-        int firstPN = message.indexOf("<ProjectName>") + 13;
-        int lastPN =  message.lastIndexOf("<ProjectName>");
-        String projectName = message.substring(firstPN, lastPN);
-        int firstSD = message.indexOf("<Sender>") + 8;
-        int lastSD =  message.lastIndexOf("<Sender>");
-        String sender = message.substring(firstSD, lastSD);
-        result[0] = projectName;
-        result[1] = sender;
+        result[0] = readTag(message, projectTag);
+        result[1] = readTag(message, senderTag);
         return result;
     }
 //    public static void main(String[] args) {
