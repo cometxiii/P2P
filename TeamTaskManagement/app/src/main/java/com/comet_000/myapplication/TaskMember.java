474a48d80bad42d52be2738c38d3cc1083fea222
Lang <bnlinhlang@gmail.com>
2015-05-19 01:44:12 +0700

    Add load mail function to synchronize button.

--- TeamTaskManagement/app/src/main/java/com/comet_000/myapplication/TaskMember.java
+++ TeamTaskManagement/app/src/main/java/com/comet_000/myapplication/TaskMember.java
@@ -1,8 +1,12 @@
 package com.comet_000.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -16,12 +20,25 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Flags;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Store;
+import javax.mail.search.AndTerm;
+import javax.mail.search.BodyTerm;
+import javax.mail.search.FlagTerm;
 
 
 public class TaskMember extends ActionBarActivity {
@@ -32,6 +49,8 @@ public class TaskMember extends ActionBarActivity {
     DatabaseHelper dbHelper;
     DataProvider dataProvider = new DataProvider();
     String loadProjectName, loadAccount, loadPassword, loadOwner;
+    MailManager mailManager;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +62,7 @@ public class TaskMember extends ActionBarActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
-
+        progressDialog = new ProgressDialog(TaskMember.this);
         TabHost tabHost=(TabHost)findViewById(R.id.tabHost);
         Intent intent=getIntent();
 
@@ -143,7 +162,8 @@ public class TaskMember extends ActionBarActivity {
         /////////////////////////////////////////////////////////////////
         //REFRESH here
         if(id==R.id.synchronize){
-
+            MailChecker checkMailTask = new MailChecker();
+            checkMailTask.execute();
         }
         return super.onOptionsItemSelected(item);
     }
@@ -243,5 +263,242 @@ public class TaskMember extends ActionBarActivity {
     private String getProjectDescriptions(){
         TableProject myProject = dataProvider.getProject(loadProjectName, loadOwner);
         return myProject.getProjectDescriptions();
+    }
+
+    //Check mail
+
+    private class MailChecker extends AsyncTask<Void, Void, String[]> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setMessage("Checking mails...");
+            progressDialog.show();
+        }
+        @Override
+        protected String[] doInBackground(Void... params) {
+            Properties props = System.getProperties();
+            props.setProperty("mail.store.protocol", "imaps");
+            try {
+                Session session = Session.getDefaultInstance(props, null);
+                //GMail
+                System.out.println("GMail logging in..");
+                Store store = session.getStore("imaps");
+                store.connect("imap.gmail.com", loadAccount, loadPassword);
+                System.out.println("Connected to = " + store);
+                Folder inbox = store.getFolder("Inbox");
+                inbox.open(Folder.READ_WRITE);
+                //Enter term to search here
+                BodyTerm bodyTerm = new BodyTerm("<zfgHsj6Uyk>");
+                FlagTerm flagTerm = new FlagTerm(new Flags(Flags.Flag.SEEN), false);
+                AndTerm andTerm = new AndTerm(bodyTerm, flagTerm);
+                //Search
+                Message[] foundMessages = inbox.search(andTerm);
+                String[] listMessage = new String[foundMessages.length];
+                System.out.println("Total P2P mails are = " + listMessage.length);
+                for (int i = foundMessages.length - 1; i >= 0; i--) {
+                    listMessage[i] = foundMessages[i].getContent().toString();
+                }
+                inbox.setFlags(foundMessages, new Flags(Flags.Flag.SEEN), true);
+                store.close();
+                return listMessage;
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String[] result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            for (String message : result)
+                alertMessage(message);
+        }
+    }
+
+    public void alertMessage(String message) {
+        String mailType = mailManager.classifyMail(message);
+        switch (mailType) {
+            case MailManager.invitationTag:
+                final String[] result = mailManager.readInvitation(message);
+                final String projectName = result[0];
+                final String projectDes = result[1];
+                final String projectOwner = result[2];
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                if (dataProvider.checkProject(projectName, projectOwner)) {
+                                    dataProvider.addProject(new TableProject(projectName, projectDes, projectOwner));
+                                    dataProvider.addProjectMember(new TableProjectMember(projectName, projectOwner, projectOwner));
+                                    dataProvider.addProjectMember(new TableProjectMember(projectName, projectOwner, loadAccount));
+                                    Toast.makeText(getApplicationContext(), "Add new project successfully!", Toast.LENGTH_SHORT).show();
+                                    String message1 = mailManager.makeAcceptInvitation(projectName, loadAccount);
+                                    MailSender myMailSender = new MailSender(projectOwner, "P2P invitation acceptance", message1, loadAccount, loadPassword, TaskMember.this);
+                                    myMailSender.send();
+                                }
+                                else {
+                                    Toast.makeText(getApplicationContext(), "This project already exist!", Toast.LENGTH_SHORT).show();
+                                }
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("You have been invited to the project " + projectName + ", do you want to join?")
+                        .setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener).show();
+                break;
+            case MailManager.acceptIviTag:
+                String[] result1 = mailManager.readAcceptInvitation(message);
+                if (dataProvider.checkProjectMember(result1[0], result1[1], loadAccount)) {
+                    dataProvider.addProjectMember(new TableProjectMember(result1[0], loadAccount, result1[1]));
+                    loadMembers();
+                    DialogInterface.OnClickListener dialogClickListener1 = new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    break;
+                            }
+                        }
+                    };
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+                    builder1.setMessage("User " + result1[1] + " has accepted your invitation to project " + result1[0] + ".")
+                            .setPositiveButton("Ok", dialogClickListener1).show();
+                }
+                break;
+            case MailManager.assignTaskTag:
+                final String[] result2 = mailManager.readAssignment(message);
+                final String projectName1 = result2[0];
+                final String owner = result2[1];
+                final String taskName = result2[2];
+                final String taskDes = result2[3];
+                if (dataProvider.checkTask(taskName, projectName1, owner)) {
+                    DialogInterface.OnClickListener dialogClickListener2 = new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    dataProvider.addTask(new TableTask(projectName1, owner, taskName, taskDes, loadAccount, "accepted"));
+                                    loadTasks();
+                                    String message = mailManager.makeAccetpTask(projectName1, taskName, loadAccount);
+                                    MailSender myMailSender = new MailSender(owner, "P2P assignment acceptance", message, loadAccount, loadPassword, TaskMember.this);
+                                    myMailSender.send();
+                                    break;
+                                case DialogInterface.BUTTON_NEGATIVE:
+                                    String messageDeny = mailManager.makeDenyTask(projectName1, taskName, loadAccount);
+                                    myMailSender = new MailSender(owner, "P2P assignment deny", messageDeny, loadAccount, loadPassword, TaskMember.this);
+                                    myMailSender.send();
+                                    break;
+                            }
+                        }
+                    };
+                    AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+                    builder2.setMessage("You have been assigned to task " + taskName + " from project " + projectName1 + ", do you want to join?")
+                            .setPositiveButton("Yes", dialogClickListener2)
+                            .setNegativeButton("No", dialogClickListener2).show();
+                }
+                break;
+            case MailManager.acceptTaskTag:
+                String[] result3 = mailManager.readAcceptTask(message);
+                if (!dataProvider.checkTaskAssignment(result3[1], result3[0], loadAccount, result3[2], "waiting")) {
+                    dataProvider.updateTaskAssignment(result3[0], result3[1], result3[2], "accepted");
+                    DialogInterface.OnClickListener dialogClickListener3 = new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    break;
+                                case DialogInterface.BUTTON_NEGATIVE:
+                                    break;
+                            }
+                        }
+                    };
+                    AlertDialog.Builder builder3 = new AlertDialog.Builder(this);
+                    builder3.setMessage("User " + result3[2] + " has accepted your assignment to task " + result3[1] + " of project " + result3[0] + ".")
+                            .setPositiveButton("Ok", dialogClickListener3).show();
+                }
+                break;
+            case MailManager.changeStaTag:
+                String[] result4 = mailManager.readChangeStatus(message);
+                if (!dataProvider.checkTaskMember(result4[1], result4[0], loadAccount, result4[2])) {
+                    dataProvider.updateTaskAssignment(result4[0], result4[1], result4[2], result4[3]);
+                    DialogInterface.OnClickListener dialogClickListener4 = new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    break;
+                                case DialogInterface.BUTTON_NEGATIVE:
+                                    break;
+                            }
+                        }
+                    };
+                    AlertDialog.Builder builder4 = new AlertDialog.Builder(this);
+                    builder4.setMessage("User " + result4[2] + " has changed status of task " + result4[1] + " of project " + result4[0] + " to " + result4[3] + ".")
+                            .setPositiveButton("Ok", dialogClickListener4).show();
+                }
+                break;
+            case MailManager.excludeTaskTag:
+                String[] result5 = mailManager.readExcludeTask(message);
+                if (!dataProvider.checkTaskMember(result5[1], result5[0], result5[2], loadAccount)) {
+                    dataProvider.deleteTask(result5[0], result5[1], result5[2]);
+                    loadTasks();
+                    DialogInterface.OnClickListener dialogClickListener5 = new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    break;
+                                case DialogInterface.BUTTON_NEGATIVE:
+                                    break;
+                            }
+                        }
+                    };
+                    AlertDialog.Builder builder5 = new AlertDialog.Builder(this);
+                    builder5.setMessage("You have been excluded from task " + result5[1] + " of project " + result5[0] + ".")
+                            .setPositiveButton("Ok", dialogClickListener5).show();
+                }
+                break;
+            case MailManager.changeDesTag:
+                String[] result6 = mailManager.readChangeDes(message);
+                dataProvider.updateTaskDes(result6[0], result6[1], result6[2], result6[3]);
+                DialogInterface.OnClickListener dialogClickListener6 = new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder builder6 = new AlertDialog.Builder(this);
+                builder6.setMessage("Description of task " + result6[1] + " from project " + result6[0] + " has been changed to " + result6[3] + ".")
+                        .setPositiveButton("Ok", dialogClickListener6).show();
+                break;
+            case MailManager.denyTaskTag:
+                final String[] result7 = mailManager.readDenyTask(message);
+                DialogInterface.OnClickListener dialogClickListener7 = new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                if(!dataProvider.checkTaskMember(result7[1], result7[0], loadAccount, result7[2])){
+                                    dataProvider.updateTaskDeny(result7[0], result7[1], loadAccount, "new", result7[2]);
+                                }
+                                else{
+                                    Toast.makeText(getApplicationContext(), "This task does not exist!", Toast.LENGTH_SHORT).show();
+                                }
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder builder7 = new AlertDialog.Builder(this);
+                builder7.setMessage("User " + result7[2] + " has denied request from task " + result7[1]+ ".")
+                        .setPositiveButton("Ok", dialogClickListener7).show();
+                break;
+
+        }
     }
 }
